da18f01411e0e619032636917b5b0dcc0d3465d7
Vicopem01 <vicopem@gmail.com>
2022-04-28 17:21:40 +0100

    bug fixes

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ const Document = () => {
         />
         <link rel="icon" href="/logo.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
